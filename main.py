7aa7b97e1e08a83926b20df46252c7b56eb28e3b
Mohammed Alshabib <56405123+MAlshabib@users.noreply.github.com>
2025-07-14 18:08:21 +0300

    CSV file now can be read

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from cybersecuty-threats!")
+import pandas as pd
 
+def main():
+    df = pd.read_csv("Global_Cybersecurity_Threats_2015-2024.csv")
+
+    print("=== Dataset Summary ===")
+    print(df.info())
+
+    print("\n=== First Few Records ===")
+    print(df.head())
+
+    print("\n=== Missing Values Check ===")
+    print(df.isnull().sum())
+
+    print("\n=== Summary Statistics ===")
+    print(df.describe(include='all'))
 
 if __name__ == "__main__":
     main()
